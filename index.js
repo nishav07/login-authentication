367f11df512124f98bcea03de9938250ea0fe8b6
unknown <nishavkumar07@gmail.com>
2025-07-30 16:02:36 +0530

    added to github

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const port = 3000;
+
+app.set("view engine","ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.set("views",path.join(__dirname,"views"));
+
+app.listen(port,() => {
+    console.log(`app running at localhost:${3000}`)
+})
+
+app.get("/" , (req,res) => {
+    res.render("index")
+})
