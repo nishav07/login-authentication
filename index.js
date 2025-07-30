acf701079a6574cd37c0906d01f0298542516869
unknown <nishavkumar07@gmail.com>
2025-07-30 21:34:00 +0530

    signup auth completed

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
+const sql = require('mysql2');
+require('dotenv').config();
 const path = require('path')
 const port = 3000;
-
 app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.set("views",path.join(__dirname,"views"));
+
+
+const pool = sql.createPool({
+  host:process.env.DB_HOST,
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+  port:process.env.DB_PORT
+})
+
+pool.query("SELECT 1 + 1 AS result", (err, results) => {
+  if (err) {
+    console.error("❌ Error connecting:", err.message);
+  } else {
+    console.log("✅ Connected! Result:", results[0].result);
+  }
+});
+
 
 app.listen(port,() => {
     console.log(`app running at localhost:${3000}`)
@@ -27,9 +46,22 @@ app.post("/signup",(req,res) => {
     let email = data.email;
     let password = data.password;
     console.log(username,password,email);
+
+    pool.query(`INSERT INTO users (username,email,password) VALUES(?,?,?)`,[username,email,password], (err,result) => {
+      if(err) throw err
+      else console.log("data inserted succefully");
+    })
     res.redirect("/")
 })
 
 app.get("/login" , (req,res) => {
     res.render("login");
+})
+
+app.post("/login" , (req,res) => {
+    console.log(req.body);
+    let data = req.body;
+    let username = data.username;
+    let password = data.password;
+    console.log(username,password,email);
 })
